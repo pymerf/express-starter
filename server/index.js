d3656b430b189c3b471c7c7db2232ba44c415c27
Blessed Sibanda <pymerf@gmail.com>
2024-02-11 14:26:04 +0200

    Create user model and connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
+import mongoose from "mongoose";
 import config from "./config";
 import app from "./app";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, { autoIndex: true });
+
+mongoose.connection.on("error", () => {
+  throw new Error(`Unable to connect to database: ${config.mongoUri}`);
+});
 
 app.listen(config.port, (err) => {
   if (err) console.error(err);
---
+++ server/models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+import uniqueValidator from "mongoose-unique-validator";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: "Email already exists",
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+      required: "Email is required",
+    },
+    hashedPassword: {
+      type: String,
+      required: "Password is required",
+    },
+    salt: String,
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods = {
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) == this.hashedPassword;
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()).toString();
+  },
+};
+
+UserSchema.virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+UserSchema.path("hashedPassword").validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate("password", "Password must be at least 6 characters.");
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate("password", "Password is required");
+  }
+}, null);
+
+UserSchema.plugin(uniqueValidator);
+
+export default mongoose.model("User", UserSchema);
